0ef6c6ed949fc3dde30b395be20fbb35e85ef41f
renatoexpert <renatorraraujo@gmail.com>
2024-03-01 11:50:06 -0300

    new hello world project

---
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -0,0 +1,13 @@
+package com.shogunautomacao.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
