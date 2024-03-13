203159699b188e93fbfb54592021d4d811555fe4
renatoexpert <renatorraraujo@gmail.com>
2024-03-13 11:57:50 -0300

    testing import

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -1,13 +1,30 @@
 package com.shogunautomacao.app;
 
+//import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+//import org.eclipse.milo.opcua.sdk.client.*;
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfig;
+//import org.eclipse.milo.opua.stack.core.types.enumerated.MessageSecurityMode;
+//import org.eclipse.milo.opua.stack.core.security.SecurityPolicy;
+//import org.apache.commons.math3.*;
+//import org.joda.time.DateTime;
+
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
+	String endpoint = "opc.tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
+/*
+	OpcUaClientConfig client_config = OpcUaClientConfig.builder()
+	       .setEndpoint(getEndpoint(endpoint))
+                .setSecurityPolicy(SecurityPolicy.None)
+                .setSecurityMode(MessageSecurityMode.None)
+                .build();
+*/
         System.out.println( "Hello World!" );
     }
 }
