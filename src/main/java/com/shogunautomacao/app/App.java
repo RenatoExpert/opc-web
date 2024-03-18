2967548839c7b795816f5707fcbff2e0ca939fbb
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-18 15:00:10 -0300

    now with ua exception

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -1,12 +1,17 @@
 package com.shogunautomacao.app;
 
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.stack.core.UaException;
 
 public class App {
-    public static void main( String[] args ) {
-	String endpoint_url = "opc.tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
-	OpcUaClient client = OpcUaClient.create(endpoint_url);
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        String endpoint_url = "opc.tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
+        try {
+            OpcUaClient client = OpcUaClient.create(endpoint_url);
+            System.out.println("Client initialized successfully!");
+        } catch (UaException e) {
+            System.err.println("Failed to initialize client: " + e.getMessage());
+        }
     }
 }
 
