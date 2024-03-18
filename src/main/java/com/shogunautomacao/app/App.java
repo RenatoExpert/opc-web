f2312ff21c87294e72eecd59d17932a163e2a29c
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-18 14:20:03 -0300

    Revert "import KeyStoreLoader"
    This reverts commit 7461be3788d28552499e32b94390cf5508c98dfe.

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -1,7 +1,6 @@
 package com.shogunautomacao.app;
 
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
-import org.eclipse.milo.opcua.sdk.client.KeyStoreLoader;
 import org.eclipse.milo.opcua.sdk.client.api.identity.AnonymousProvider;
 import org.eclipse.milo.opcua.sdk.client.api.identity.IdentityProvider;
 import org.eclipse.milo.opcua.stack.core.security.SecurityPolicy;
