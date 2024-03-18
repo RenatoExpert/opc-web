8691f777ea1bd203de2ec766494be2dcb5d7fff7
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-18 13:43:19 -0300

    making the same imports as client example

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -1,5 +1,11 @@
 package com.shogunautomacao.app;
 
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.api.identity.AnonymousProvider;
+import org.eclipse.milo.opcua.sdk.client.api.identity.IdentityProvider;
+import org.eclipse.milo.opcua.stack.core.security.SecurityPolicy;
+import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
+
 public class App {
     public static void main( String[] args ) {
 	String endpoint = "opc.tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
