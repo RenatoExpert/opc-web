7461be3788d28552499e32b94390cf5508c98dfe
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-18 14:16:50 -0300

    import KeyStoreLoader

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -1,6 +1,7 @@
 package com.shogunautomacao.app;
 
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.KeyStoreLoader;
 import org.eclipse.milo.opcua.sdk.client.api.identity.AnonymousProvider;
 import org.eclipse.milo.opcua.sdk.client.api.identity.IdentityProvider;
 import org.eclipse.milo.opcua.stack.core.security.SecurityPolicy;
