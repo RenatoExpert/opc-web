4e16e6194c2266426bc052c58670eb7383f7e928
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-18 14:47:36 -0300

    fix import client

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -1,5 +1,7 @@
 package com.shogunautomacao.app;
 
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+
 public class App {
     public static void main( String[] args ) {
 	String endpoint_url = "opc.tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
