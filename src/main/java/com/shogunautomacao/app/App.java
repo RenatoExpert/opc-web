e542871875ba4554c2878cd1b7c1951e743692a2
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-18 13:41:50 -0300

    trying to instance OpcUaClient directly

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -2,9 +2,11 @@ package com.shogunautomacao.app;
 
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfig;
+import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfigBuilder;
 import org.eclipse.milo.opcua.stack.core.types.enumerated.MessageSecurityMode;
 import org.eclipse.milo.opcua.stack.core.security.SecurityPolicy;
-
+import org.eclipse.milo.opcua.stack.core.UaException;
+import org.eclipse.milo.opcua.stack.client.UaStackClient;
 
 
 /**
@@ -14,6 +16,10 @@ import org.eclipse.milo.opcua.stack.core.security.SecurityPolicy;
 public class App {
     public static void main( String[] args ) {
 	String endpoint = "opc.tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
+	OpcUaClientConfig config = new OpcUaClientConfig();
+	UaStackClient stackclient = new UaStackClient();
+	OpcUaClient client = OpcUaClient(config, stackclient);
+	//OpcUaClientConfig config = OpcUaClientConfigBuilder();
 /*
 	OpcUaClientConfig client_config = OpcUaClientConfig.builder()
 	       .setEndpoint(getEndpoint(endpoint))
