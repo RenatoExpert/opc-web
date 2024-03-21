c16c27f83577420491aa0218d93d5c50665818b4
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-21 15:12:07 -0300

    make an for each on ascii

--- src/main/java/com/shogunautomacao/app/Display.java
+++ src/main/java/com/shogunautomacao/app/Display.java
@@ -1,16 +1,25 @@
 package com.shogunautomacao.app;
 
+import java.util.ArrayList;
+
 public class Display {
-	public static void integer(int input) {
-		String hex = String.format("%02x", input);
-		System.out.println(hex);
+	private static void compare(String before, String after) {
+		String message = String.format("%s => %s", before, after);
+		System.out.println(message);
+	}
+	public static void integer(int num) {
+		String hex = String.format("%02x", num);
+		compare(Integer.toString(num), hex);
 	}
 	public static void ascii(String text) {
+		ArrayList<String> hexs = new ArrayList<String>();
 		for(char symbol: text.toCharArray()) {
 			int value = symbol;
 			String hex = String.format("%02x", value);
-			System.out.println(hex);
+			hexs.add(hex);
 		}
+		String result = String.join(" ", hexs);
+		compare(text, result);
 	}
 }
 
