e192f8ea72e9b621dc0d3b17c94503cf3f7a6713
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-22 15:59:02 -0300

    parsing url into chunk

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -7,8 +7,8 @@ import java.util.Arrays;
 public class App {
 	public static void main(String[] args) throws Exception {
 		String url = "opc.tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
-		Chunk chunk = new Chunk();
-
+		Chunk chunk = new Chunk(url);
+		chunk.get_chunk();
 	}
 }
 
