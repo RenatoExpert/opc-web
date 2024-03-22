5355e3a2b5d49dd801f3e805f0b3f5391e119d76
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-22 16:52:40 -0300

    fix host address

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class App {
 	public static void main(String[] args) throws Exception {
 		String protocol = "opc.tcp";
-		String host = "c2-3-93-58-9.compute-1.amazonaws.com";
+		String host = "ec2-3-93-58-9.compute-1.amazonaws.com";
 		int port = 4840;
 		String url = String.format("%s://%s:%d/", protocol, host, port);
 		Chunk chunk = new Chunk(url);
