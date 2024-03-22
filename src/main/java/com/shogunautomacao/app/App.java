edf95a44d3de42f10f22b90260ed65842e6b698c
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-22 15:22:12 -0300

    display message values

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -11,16 +11,24 @@ public class App {
 		System.out.println("Protocol Version");
 		byte[] protocol_version = encoder.integer(0);
 		encoder.display(protocol_version);
+
 		System.out.println("Receiver Buffer Size");
 		byte[] receiver_size = encoder.integer(8192);
+		encoder.display(receiver_size);
+
 		System.out.println("Send Buffer Size");
 		byte[] send_size = encoder.integer(8192);
 		System.out.println(Arrays.toString(send_size));
 		encoder.display(send_size);
+
 		System.out.println("Message Size");
 		byte[] message_size = encoder.integer(0);
+		encoder.display(message_size);
+
 		System.out.println("Max Chunk");
 		byte[] max_chunk = encoder.integer(0);
+		encoder.display(max_chunk);
+
 		System.out.println("Endpoint URL");
 		String url = "opc.tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
 		byte[] endpoint = encoder.string(url);
