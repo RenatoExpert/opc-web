7cd4919b32fd97fc444a568c3cb1877d88ad84c8
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-22 16:46:01 -0300

    fix string format and port var type

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -7,8 +7,8 @@ public class App {
 	public static void main(String[] args) throws Exception {
 		String protocol = "opc.tcp";
 		String host = "c2-3-93-58-9.compute-1.amazonaws.com";
-		String port = 4840;
-		String url = String.format("%s://%s:%i/", protocol, host, port);
+		int port = 4840;
+		String url = String.format("%s://%s:%d/", protocol, host, port);
 		Chunk chunk = new Chunk(url);
 		byte[] hello = chunk.get_chunk();
 		Socket socket = new Socket(host, port);
