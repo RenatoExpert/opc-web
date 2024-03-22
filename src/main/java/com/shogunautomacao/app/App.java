efa93e89eba33979c262b7280f68de35f422cd4b
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-22 15:57:48 -0300

    removing comments

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -9,21 +9,6 @@ public class App {
 		String url = "opc.tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
 		Chunk chunk = new Chunk();
 
-		/*
-		Encoder encoder = new Encoder();
-		//	Header
-		System.out.println("Message type");
-		byte[] message_type = encoder.ascii("HEL");
-		encoder.display(message_type);
-
-		System.out.println("Reserved Byte");
-		byte[] reserved_byte = encoder.ascii("F");
-		encoder.display(reserved_byte);
-
-		System.out.println("Message Size");
-		byte[] message_size = encoder.integer(52);
-		//	Calculate length
-		//	*/
 	}
 }
 
