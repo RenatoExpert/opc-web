4fbe48063c217f9c47594dd2880a20d4128e9d3f
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-22 17:09:16 -0300

    adding output objects

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -3,6 +3,8 @@ package com.shogunautomacao.app;
 import com.shogunautomacao.app.Chunk;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.io.IOException;
 
 public class App {
@@ -16,6 +18,8 @@ public class App {
 
 		System.out.println("\nConnecting to " + url);
 		try (Socket socket = new Socket(host, port)) {
+			OutputStream output = socket.getOutputStream();
+			PrintWriter writer = new PrintWriter(output, true);
 		} catch (UnknownHostException ex) {
 			System.out.println("Server not found: " + ex.getMessage());
 		} catch (IOException ex) {
