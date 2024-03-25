7e565ec10be2799f9bc8d3bd230d83fec51b0ceb
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-25 09:02:07 -0300

    trying to connect to localhost

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -14,7 +14,8 @@ import java.io.IOException;
 public class App {
 	public static void main(String[] args) throws Exception {
 		String protocol = "opc.tcp";
-		String host = "ec2-3-93-58-9.compute-1.amazonaws.com";
+		//String host = "ec2-3-93-58-9.compute-1.amazonaws.com";
+		String host = "localhost";
 		int port = 4840;
 		String url = String.format("%s://%s:%d/", protocol, host, port);
 		Chunk chunk = new Chunk(url);
@@ -31,7 +32,8 @@ public class App {
 			System.out.println("\nReading from server...");
 			Encoder encoder = new Encoder();
 			while((line = reader.readLine()) != null) {
-				encoder.display(line);
+				System.out.println("Test");
+				System.out.println(line);
 			}
 		} catch (UnknownHostException ex) {
 			System.out.println("Server not found: " + ex.getMessage());
