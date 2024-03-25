7362a863fe14298fcb1ad0cd2468611f58d2594d
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-03-25 17:49:33 -0300

    removing import of PrintWriter

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -7,7 +7,6 @@ import java.net.UnknownHostException;
 import java.io.OutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.io.PrintStream;
 import java.io.BufferedReader;
 import java.io.IOException;
